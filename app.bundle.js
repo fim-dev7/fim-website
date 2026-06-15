b02d9b599d479e0098f51e38fb576247910a7950
Hoang Anh Ngo <theango@Theas-Laptop.local>
2026-06-16 09:06:29 +1000

    polish(home): drop dangling separator when an episode has no tags

--- app.bundle.js
+++ app.bundle.js
@@ -1405,7 +1405,7 @@ function EpisodeCard({
     className: "epc"
   }, React.createElement("div", {
     className: "epc-tag"
-  }, "Ep ", ep.n, " \xB7 ", ep.tags.join(" · ")), React.createElement("h3", {
+  }, "Ep ", ep.n, ep.tags && ep.tags.length ? " · " + ep.tags.join(" · ") : ""), React.createElement("h3", {
     className: "epc-title"
   }, ep.title.split(/\s*\|\s*/)[0]), React.createElement("p", {
     className: "epc-desc"
